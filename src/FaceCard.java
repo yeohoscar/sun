b97367f5caeba666add7a8b27c29cf40acc4f508
YanhaoSun <sunyanhao134@gmail.com>
2023-03-21 17:59:57 +0000

    GameOfBlackJack controller added

---
+++ src/FaceCard.java
@@ -0,0 +1,16 @@
+// This package provides classes necessary for implementing a game system for playing poker
+
+
+public class FaceCard extends Card
+{
+    //--------------------------------------------------------------------//
+    //--------------------------------------------------------------------//
+    // Constructors
+    //--------------------------------------------------------------------//
+    //--------------------------------------------------------------------//
+
+    public FaceCard(String face, String suit, int rank)	{
+        super(face, suit, rank);
+    }
+
+}
---
+++ src/GameOfBlackJack.java
@@ -0,0 +1,63 @@
+public class GameOfBlackJack {
+    private Player[] players;
+
+    private DeckOfCards deck;
+
+    private int numPlayers;
+    public GameOfBlackJack(String[] names, int bank)
+    {
+        numPlayers = names.length;
+
+        players = new Player[numPlayers];
+
+        for (int i = 0; i < numPlayers; i++)
+            if (i == 0)
+                players[i] = new HumanPlayer(names[i].trim(), bank);
+            else
+                players[i] = new ComputerPlayer(names[i].trim(), bank);
+
+        deck  = new DeckOfCards();
+    }
+
+    public void play(){
+        while (getNumSolventPlayers() > 1) {
+            RoundOfPoker round = new RoundOfPoker(deck, players);
+
+            round.play();
+
+            try {
+                System.out.print("\n\nPlay another round? Press 'q' to terminate this game ... ");
+
+                byte[] input = new byte[100];
+
+                System.in.read(input);
+
+                for (int i = 0; i < input.length; i++)
+                    if ((char)input[i] == 'q' || (char)input[i] == 'Q')
+                        return;
+            }
+            catch (Exception e) {};
+        }
+    }
+    public int getNumSolventPlayers() {
+        // how many players still have money left?
+
+        int count = 0;
+
+        for (int i = 0; i < getNumPlayers(); i++)
+            if (getPlayer(i) != null && !getPlayer(i).isBankrupt())
+                count++;
+
+        return count;
+    }
+    public int getNumPlayers() {
+        return numPlayers;
+
+    }
+    public Player getPlayer(int num) {
+        if (num >= 0 && num <= numPlayers)
+            return players[num];
+        else
+            return null;
+    }
+}
