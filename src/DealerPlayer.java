82d55f985bd94cc4c0b5b32858baa1c6a69ea0da
Oscar Yeoh <oscar.yeoh@ucdconnect.ie>
2023-03-22 18:36:30 +0000

    Edited DealerPlayer to accomodate changes in Player

--- src/DealerPlayer.java
+++ src/DealerPlayer.java
@@ -1,20 +1,24 @@
+import poker.Card;
+
 public class DealerPlayer extends Player{
+    private static final int DEALER_STAND_LIMIT = 17;
+
     public DealerPlayer(String name, int bank)
     {
         super(name, bank);
     }
     public int getFaceUp()
     {
-        return this.getHand()[0].getCard(0).getValue();
+        return this.getHand().get(0).getCard(0).getValue();
     }
     public Card showOneCard()
     {
-        return this.getHand()[0].getCard(0);
+        return this.getHand().get(0).getCard(0);
     }
     @Override
-    Action chooseAction(){
+    Action chooseAction(BlackjackHand hand){
         try{
-            if(this.getHand()[0].getValue()<17)
+            if(this.getHand().get(0).getValue()<DEALER_STAND_LIMIT)
             {
                 return Action.HIT;
             }
@@ -41,35 +45,10 @@ public class DealerPlayer extends Player{
         // 然后按照输赢决定赌注去向, 开始下一轮
         return null;
     }
-    @Override
-    boolean hit(BlackjackHand hand) {
-        hand.addCard();
-        if(hand.getValue()<17) {
-            return false;
-        }
-        else if(hand.getValue()>21){
-            return false;
-        }
-        else {
-            return false;
-        }
-    }
     //TODO: we need to add an action BUST?
 //    @Override
 //    boolean bust(BlackjackHand hand)
 //    {
 //            return true;
 //    }
-    @Override
-    boolean stand(BlackjackHand hand){
-            return true;
-    }
-    @Override
-    boolean split(BlackjackHand hand){
-        return false;
-    }
-    @Override
-    boolean doubleDown(BlackjackHand hand){
-        return false;
-    }
 }
