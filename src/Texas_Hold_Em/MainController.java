cdd6bbad10b8c457b0d11148a0cecc0e86e2acf2
Haocheng12 <121132896+Haocheng12@users.noreply.github.com>
2023-03-31 14:08:34 +0100

    Add abstract class MainController
    Add mainController
    change players from array to list
    delete dealer class
    completed TexasController

--- src/Texas_Hold_Em/MainController.java
+++ src/Texas_Hold_Em/MainController.java
@@ -1,56 +1,48 @@
 package Texas_Hold_Em;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public abstract class MainController {
 
-    protected Player[] players;
+    protected ArrayList<Player> players;
 
     protected Deck deck;
 
     protected int numPlayers;
 
-    protected HashMap<String,Integer> Pots;
-
-    public int getNumPlayers() {
-        return numPlayers;
-
-    }
-
 
     public Player getPlayer(int num) {
         if (num >= 0 && num <= numPlayers)
-            return players[num];
+            return players.get(num);
         else
             return null;
     }
 
 
-    public int getNumSolventPlayers() {
-        // how many players still have money left?
+    public void setUp(String[] names, int bank) {
+        players = new ArrayList<>();
+        numPlayers = names.length;
 
-        int count = 0;
 
-        for (int i = 0; i < getNumPlayers(); i++)
-            if (getPlayer(i) != null && !getPlayer(i).isBankrupt())
-                count++;
-
-        return count;
+        for (int i = 0; i < numPlayers; i++)
+            if (i == 0)
+                players.add(new HumanPlayer(names[i].trim(), bank));
+            else
+                players.add(new ComputerPlayer(names[i].trim(), bank));
     }
 
-    public void play()	{
-        int dealerIndex=numPlayers-1;
-        int numSolventPlayers = players.length;
-        while (getNumSolventPlayers() > 1) {
-            deck.reset();//before each game starts, shuffle the deck
-            numSolventPlayers=getNumSolventPlayers();
 
-            //every player can act as dealer, dealer also needs to place bet, but dealer is the last one to place bet
-            players[dealerIndex] = new DealerPlayer(players[dealerIndex].getName(), players[dealerIndex].getBank());
-            players[dealerIndex].setDealer(true);
+    public void play()	{
+        int dealerIndex=numPlayers;
+        while (players.size() > 1) {
+            deck.reset();//before each game starts, shuffle the deck
+            //next player become a dealer
+            dealerIndex++;
+            if(dealerIndex==numPlayers){dealerIndex=0;}
 
             //start a game, there are four rounds within a game: Pre-flop, Turn, River and the one after River.
-            RoundsOfTexas round = new RoundsOfTexas(deck, players, dealerIndex, numSolventPlayers);
+            RoundsOfTexas round = new RoundsOfTexas(deck, players, dealerIndex);
             round.play();
 
             try {
@@ -68,5 +60,8 @@ public abstract class MainController {
             catch (Exception e) {};
 
         }
+
+        System.out.println("Congratulations!");
+        System.out.println(players.get(0).getName()+" is the Winner of the Game!");
     }
 }
--- src/Texas_Hold_Em/TexasController.java
+++ src/Texas_Hold_Em/TexasController.java
@@ -10,61 +10,9 @@ package Texas_Hold_Em;
 public class TexasController extends MainController
 {	
 
-	
-	public TexasController(String[] names, int bank) {
-
-		numPlayers = names.length;
-		
-		players = new Player[numPlayers];
-		
-		for (int i = 0; i < numPlayers; i++)
-			if (i == 0)
-				players[i] = new HumanPlayer(names[i].trim(), bank);
-			else
-				players[i] = new ComputerPlayer(names[i].trim(), bank);
-		
+	@Override
+	public void setUp(String[] names, int bank) {
+		super.setUp(names, bank);
 		deck  = new DeckOfTexasCards();
 	}
-
-	//--------------------------------------------------------------------//
-	//--------------------------------------------------------------------//
-	// Play Poker
-	//--------------------------------------------------------------------//
-	//--------------------------------------------------------------------//
-
-
-
-
-	//--------------------------------------------------------------------//
-	//--------------------------------------------------------------------//
-	// Launcher for application
-	//--------------------------------------------------------------------//
-	//--------------------------------------------------------------------//
-
-	/*public static void main(String[] args) {
-		String[] names = {"Human", "Tom", "Dick", "Harry"};
-
-		System.out.println("\nWelcome to the Automated Poker Machine ...\n\n");
-
-		System.out.print("\nWhat is your name?  ");
-
-		byte[] input = new byte[100];
-
-		try {
-			System.in.read(input);
-
-			names[0] = new String(input);
-		}
-		catch (Exception e){};
-
-		int startingBank = 10;
-
-		System.out.println("\nLet's play POKER ...\n\n");
-
-		GameOfPoker game = new GameOfPoker(names, startingBank);
-
-		game.play();
-	}*/
-	
-
 }
