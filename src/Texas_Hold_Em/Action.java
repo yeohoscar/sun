bf6bb1ce07a39dcc9a581a00c43e899938b284a0
Oscar Yeoh <oscar.yeoh@ucdconnect.ie>
2023-04-11 20:53:38 +0100

    added choose num players

--- src/Texas_Hold_Em/Action.java
+++ src/Texas_Hold_Em/Action.java
@@ -1,5 +1,7 @@
 package Texas_Hold_Em;
 
+//Represents the actions a player can take
+
 public enum Action {
     SEE,
     RAISE,
--- src/Texas_Hold_Em/TexasController.java
+++ src/Texas_Hold_Em/TexasController.java
@@ -9,8 +9,10 @@ package Texas_Hold_Em;
 
 import poker.DeckOfCards;
 
-public class TexasController extends MainController
-{	
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TexasController extends MainController {
 
 	@Override
 	public void setUp(String[] names, int bank) {
@@ -18,5 +20,48 @@ public class TexasController extends MainController
 		deck  = new DeckOfCards();
 	}
 
+	public static void main(String[] args) {
+		String[] names = {"Human", "Tom", "Dick", "Harry", "Jim", "Dave", "Paul", "Bob", "John", "Bill"};
 
+		System.out.println("\nWelcome to the Automated Texas Hold'Em Machine ...\n\n");
+
+		System.out.print("\nWhat is your name?  ");
+
+		byte[] input = new byte[100];
+
+		try {
+			int numBytesRead = System.in.read(input);
+			String userInput = new String(input, 0, numBytesRead).trim();
+			if (!userInput.isEmpty()) {
+				names[0] = userInput;
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		System.out.print("\nHow many players do you wish to play with?  ");
+
+		Scanner scanner = new Scanner(System.in);
+		String[] playerNames = names;
+
+		try {
+			int numPlayers = scanner.nextInt();
+
+			if (numPlayers < 10 && numPlayers > 2) {
+				playerNames = Arrays.copyOfRange(names, 0, numPlayers);
+			}
+
+		} catch (Exception e) {
+			System.out.println("Invalid input.");
+			scanner.nextLine(); // Clear the scanner buffer
+		}
+
+		int startingBank = 100;
+
+		System.out.println("\nLet's play Texas Hold'Em ...\n\n");
+		TexasController game = new TexasController();
+		game.setUp(playerNames, startingBank);
+		game.play();
+	}
 }
