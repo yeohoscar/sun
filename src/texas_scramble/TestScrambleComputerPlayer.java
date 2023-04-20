a28da39cbc3a91586fb0fe94987c5dc8d7016835
YanhaoSun <sunyanhao134@gmail.com>
2023-04-20 12:00:27 +0100

    findHighestScoreWord updated

--- src/texas_scramble/TestScrambleComputerPlayer.java
+++ src/texas_scramble/TestScrambleComputerPlayer.java
@@ -20,4 +20,17 @@ public class TestScrambleComputerPlayer {
         String[] letters4 = {"Z", "Z", "Z", "S"};
         player1.findAllWords(letters4);
     }
+    @Test
+    public void testFindHighestScoreWord(){
+        ComputerScramblePlayer player1 = new ComputerScramblePlayer("Tom", 0, 0);
+        String[] letters1 = {"e", "n", "o", "p", "g", "i", "a"};
+        String[] letters2 = {"A", "S", "S"};
+
+        //test letters3 and letters4 first, these two should have worked
+        String[] letters3 = {"U", "N", "O", "S"};
+        String[] letters4 = {"A", "G", "Z", "S", "M", "P", "Q"};
+        //player1.findAllWords(letters4);
+        String maxScoreWord = player1.findHighestScoreWord(letters4);
+        System.out.println("max = "+maxScoreWord);
+    }
 }
