e336bdc2a91e511c2e544ac98b71dab5bd4b66cd
YanhaoSun <sunyanhao134@gmail.com>
2023-04-28 14:07:58 +0100

    add comments for ComputerScramblePlayer, TexasPlayer, DictionaryTrie, PrintScramble and TestScrambleComputerPlayer

--- src/texas/scramble/dictionary/DictionaryTrie.java
+++ src/texas/scramble/dictionary/DictionaryTrie.java
@@ -107,14 +107,6 @@ public class DictionaryTrie {
         return false;
     }
     public String[] removeBlank(String[] letters){
-//		ArrayList<String> availableLetters = findAvailableLetters(letters);
-//		HashMap<String, Integer> temp = new HashMap<>(deckOfTiles.getAllTiles());
-//		for(int i=0; i<letters.length; i++){
-//			if(letters[i].equals(" ")){
-//				letters[i]=findHighestScoreLetter(availableLetters, temp);
-//				availableLetters = updateAvailableLetters(temp);
-//			}
-//		}
         ArrayList<String> temp = new ArrayList<>();
         for(String letter: letters){
             if(!letter.equals(" ")){
@@ -129,22 +121,10 @@ public class DictionaryTrie {
         if(containBlank(letters)){
             letters = removeBlank(letters);
         }
-//        for(String letter: letters){
-//            System.out.println("removed blank letter = "+letter);
-//        }
         List<List<String>> allWords = new ArrayList<>();
         current = root;
         for(Node child: current.children){
-//            Node n = current.children.stream()
-//                    .filter(node -> letter.charAt(0) == node.getLetter())
-//                    .findFirst()
-//                    .orElse(null);
-//            if(n==null){
-//                return new ArrayList<>();
-//            }
-            //current = n;
             String str = String.valueOf(child.letter);
-            //System.out.println("root children's letter = "+str);
             List<String> words = dfs(child, str, letters, new ArrayList<>());
             allWords.add(words);
         }
@@ -156,47 +136,8 @@ public class DictionaryTrie {
             result.addAll(word);
         }
         return result;
-        /*for(String letter: letters){
-            if(lettersContained.containsKey(letter)){
-                lettersContained.put(letter, lettersContained.get(letter)+1);
-            }else {
-                lettersContained.put(letter, 1);
-            }
-        }
-        List<List<String>> allWords = new ArrayList<>();
-        for(String letter: letters){
-            current = root;
-            Node n = current.children.stream()
-                    .filter(node -> letter.charAt(0) == node.getLetter())
-                    .findFirst()
-                    .orElse(null);
-            if(n==null){
-                return new ArrayList<>();
-            }
-            current = n;
-            allWords.add(dfs(current, letter, lettersContained, new ArrayList<>()));
-        }
-
-        List<String> result = new ArrayList<>();
-        for(List<String> words: allWords){
-            result.addAll(words);
-        }
-        return result;*/
     }
     private boolean wordContainsLetters(String prefix, String[] letters){
-//        String[] strArray = prefix.split("");
-//        boolean contained = true;
-//        HashMap<String, Integer> prefixHash = new HashMap<>();
-//        for(String letter: strArray){
-//            if(prefixHash.containsKey(letter)){
-//                prefixHash.put(letter, prefixHash.get(letter)+1);
-//            }else {
-//                prefixHash.put(letter, 1);
-//            }
-//        }
-        //compare if both key and value of prefixHash are subset of lettersContaine
-//        System.out.println("prefix = "+prefix);
-//        exit(0);
         int countNumberOfLettersContained = 0;
         for(String letter: letters){
             if(prefix.contains(letter)){
@@ -210,66 +151,17 @@ public class DictionaryTrie {
         }
         else if(letters.length==3){
             return (countNumberOfLettersContained >= 0 && countNumberOfLettersContained <= 3) && prefix.length() <= (countNumberOfLettersContained + 4);
-//            if(countNumberOfLettersContained==1 && prefix.length()<=5){
-//                return true;
-//            }else if(countNumberOfLettersContained==2 && prefix.length()<=6){
-//                return true;
-//            }else return countNumberOfLettersContained == 3 && prefix.length() <= 7;
         }else if(letters.length==4){
             return (countNumberOfLettersContained >= 0 && countNumberOfLettersContained <= 4) && prefix.length() <= (countNumberOfLettersContained + 3);
-//            if(countNumberOfLettersContained==1 && prefix.length()<=4){
-//                return true;
-//            }else if(countNumberOfLettersContained==2 && prefix.length()<=5){
-//                return true;
-//            }else if(countNumberOfLettersContained==3 && prefix.length()<=6) {
-//                return true;
-//            }else return countNumberOfLettersContained == 4 && prefix.length() <= 7;
         }else if(letters.length==5){
             return (countNumberOfLettersContained >= 0 && countNumberOfLettersContained <= 5) && prefix.length() <= (countNumberOfLettersContained + 2);
-//            if(countNumberOfLettersContained==1 && prefix.length()<=3){
-//                return true;
-//            }else if(countNumberOfLettersContained==2 && prefix.length()<=4){
-//                return true;
-//            }else if(countNumberOfLettersContained==3 && prefix.length()<=5){
-//                return true;
-//            }else if(countNumberOfLettersContained==4 && prefix.length()<=6){
-//                return true;
-//            }else return countNumberOfLettersContained == 5 && prefix.length() <= 7;
         }else if(letters.length==6){
             return (countNumberOfLettersContained >= 0 && countNumberOfLettersContained <= 6) && prefix.length() <= (countNumberOfLettersContained + 1);
-//            if(countNumberOfLettersContained==1 && prefix.length()<=2){
-//                return true;
-//            }else if(countNumberOfLettersContained==2 && prefix.length()<=3){
-//                return true;
-//            }
         }else if(letters.length==7){
             return (countNumberOfLettersContained >= 0 && countNumberOfLettersContained <= 7) && prefix.length() <= (countNumberOfLettersContained);
         }
         return false;
     }
-    /*
-
-        private boolean wordIsFormedByLettersContained(String prefix, HashMap<String, Integer> lettersContained){
-            //System.out.println("PPrefix = "+prefix);
-            String[] strArray = prefix.split("");
-            boolean contained = true;
-            HashMap<String, Integer> prefixHash = new HashMap<>();
-            for(String letter: strArray){
-                if(prefixHash.containsKey(letter)){
-                    prefixHash.put(letter, prefixHash.get(letter)+1);
-                }else {
-                    prefixHash.put(letter, 1);
-                }
-            }
-            //compare if both key and value of prefixHash are subset of lettersContained
-            for(Map.Entry<String, Integer> entry: prefixHash.entrySet()){
-                if(!(lettersContained.containsKey(entry.getKey()) && (entry.getValue()<=lettersContained.get(entry.getKey())))){
-                    contained=false;
-                }
-            }
-            return contained;
-        }
-    */
     private List<String> dfs(Node node, String prefix, String[] lettersContained, List<String> results) {
         if (node.isEndOfWord() && wordContainsLetters(prefix, lettersContained)) {
             results.add(prefix);
@@ -282,15 +174,9 @@ public class DictionaryTrie {
         }
         return results;
     }
-    /************* find all words that formed by player's current letters **************/
+    /************* find all words that only formed by player's current letters **************/
     public List<String> findAllWordsFormedByLetters(String[] letters){
         Node current;
-//        if(containBlank(letters)){
-//			letters = removeBlank(letters);
-//		}
-//        for(String letter: letters){
-//            System.out.println("letter = "+letter);
-//        }
         HashMap<String, Integer> lettersContained = new HashMap<>();
         for(String letter: letters){
             if(lettersContained.containsKey(letter)){
@@ -299,9 +185,6 @@ public class DictionaryTrie {
                 lettersContained.put(letter, 1);
             }
         }
-//        for(Map.Entry<String, Integer> entry: lettersContained.entrySet()){
-//            System.out.println("lettersContained = "+entry.getKey());
-//        }
         List<List<String>> allWords = new ArrayList<>();
         for(Map.Entry<String, Integer> entry: lettersContained.entrySet()){
             if(!entry.getKey().equals(" ")){
@@ -338,13 +221,8 @@ public class DictionaryTrie {
                 prefixHash.put(letter, 1);
             }
         }
-//        for(Map.Entry<String, Integer> entry: prefixHash.entrySet()){
-//            System.out.println("getKey = "+entry.getKey());
-//            System.out.println("getValue = "+entry.getValue());
-//        }
+
         if(lettersContained.containsKey(" ")){
-            //System.out.println("true");
-            //exit(0);
             //compare if both key and value of prefixHash are subset of lettersContained
             for(Map.Entry<String, Integer> entry: prefixHash.entrySet()){
                 if(!lettersContained.containsKey(entry.getKey()) && (entry.getValue()<=lettersContained.get(" "))){
@@ -375,7 +253,6 @@ public class DictionaryTrie {
     private List<String> dfs(Node node, String prefix, HashMap<String, Integer> lettersContained, List<String> results) {
         if (node.isEndOfWord() && wordIsFormedByLettersContained(prefix, lettersContained)) {
             results.add(prefix);
-            //System.out.println("results = "+results);
         }
         for (int i = 0; i < node.children.size(); i++) {
             Node child = node.children.get(i);
