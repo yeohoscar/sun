3c09b905bb857eb334e84be2b4c32f138ee04664
YanhaoSun <sunyanhao134@gmail.com>
2023-04-28 15:24:58 +0100

    test

--- src/texas/scramble/test/TestDeckOfTiles.java
+++ src/texas/scramble/test/TestDeckOfTiles.java
@@ -1,7 +1,7 @@
 package texas.scramble.test;
 
+import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import texas.scramble.deck.DeckOfTiles;
 
 import java.util.HashMap;
