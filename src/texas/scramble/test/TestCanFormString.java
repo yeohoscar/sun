9d0e05767107998921f8270928340219dbe5a86a
YanhaoSun <sunyanhao134@gmail.com>
2023-05-04 13:05:37 +0100

    test the error TA met

--- src/texas/scramble/test/TestCanFormString.java
+++ src/texas/scramble/test/TestCanFormString.java
@@ -5,28 +5,26 @@ import texas.scramble.deck.Tile;
 import texas.scramble.dictionary.FullDictionary;
 import texas.scramble.player.ScrambleHumanPlayer;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class TestCanFormString {
     ScrambleHumanPlayer player1 = new ScrambleHumanPlayer("Tom", 0, 0);
+    Tile[] newHand = {new Tile("P", 3),
+            new Tile("Y", 4),
+            new Tile("C", 3),
+            new Tile("A", 1),
+            new Tile("I", 1),
+            new Tile("O", 1),
+            new Tile("T", 1)};
+    String[] words = {"p-Oo", "tyaipyo", "a", "a", "a", "A", "cat", "a"};
+    String[] words2 = {"p-Oo", "tyaipyo", "a", "a", "a", "A", "CAT", "a"};
+    String[] words1 = {"CAT"};
     @Test
     public void testCanFormString(){
-        Tile[] newHand = {new Tile("P", 3),
-                          new Tile("Y", 4),
-                          new Tile("C", 3),
-                          new Tile("A", 1),
-                          new Tile("I", 1),
-                          new Tile("O", 1),
-                          new Tile("T", 1)};
-        String[] words = {"p-Oo", "tyaipyo", "a", "a", "a", "A", "cat", "a"};
-        String[] words2 = {"p-Oo", "tyaipyo", "a", "a", "a", "A", "CAT", "a"};
-        String[] words1 = {"CAT"};
-//        if(FullDictionary.getInstance().isValidWord(words1[0])){
-//            System.out.println("cat is a word");
-//        }
-//        if(player1.canFormString(newHand,words1[0])){
-//            System.out.println("cat can form string");
-//        }
         for(String word: words){
-           if (!player1.canFormString(newHand,word )||!FullDictionary.getInstance().isValidWord(word)){
+            if (!player1.canFormString(newHand,word )||!FullDictionary.getInstance().isValidWord(word)){
                System.out.println("InValid word! Please enter a word again (maximum "+newHand.length+" letters) or g to give up: ");
            }else {
                System.out.println("valid word: "+word);
@@ -39,6 +37,21 @@ public class TestCanFormString {
             }else {
                 System.out.println("valid word: "+word);
             }
+        }
+    }
+    @Test
+    public void testSimulateUserInput(){
+        String input = "cat";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        Scanner scanner = new Scanner(System.in);
+        String result = scanner.nextLine();
+        String word = result.trim().toUpperCase();
+        if (!player1.canFormString(newHand, word)||!FullDictionary.getInstance().isValidWord(word)){
+            System.out.println("InValid word! Please enter a word again (maximum "+newHand.length+" letters) or g to give up: ");
+        }else {
+            System.out.println("valid word: "+word);
         }
     }
 }
